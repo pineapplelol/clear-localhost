56b641e9191749ecf1cfaec1fa76e826e5b7180a
noa <aggarwal.neeraj141@gmail.com>
2020-12-30 19:05:58 -0800

    Add button

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,15 @@
-document.querySelector("#toggle").addEventListener("click", function () {
+document.querySelector("#enable").addEventListener("click", function () {
   chrome.runtime.sendMessage({
     toggle: true,
   });
+  document.getElementById("enable").hidden = true;
+  document.getElementById("disable").hidden = false;
+});
+
+document.querySelector("#disable").addEventListener("click", function () {
+  chrome.runtime.sendMessage({
+    toggle: true,
+  });
+  document.getElementById("enable").hidden = false;
+  document.getElementById("disable").hidden = true;
 });
